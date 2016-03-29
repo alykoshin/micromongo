620adb7bc2886d2e5260d393c6463745d233d757
Alexander <alykoshin@gmail.com>
2016-03-29 11:32:38 +0300

    query operators now supports compound field names + more tests

--- test/comparision.js
+++ test/comparision.js
@@ -17,145 +17,199 @@ var mm = require('../lib/');
 describe('#comparision operators - development', function() {
 
 
-	describe('#primitive equal (implicit $eq)', function() {
+  describe('#primitive equal (implicit $eq)', function() {
 
-		it('#same values', function() {
-			expect(mm._match( { value: 'ab' }, { value: 'ab' } )).eql(true);
-		});
+    it('#same values', function() {
+      expect(mm._match( { value: 'ab' }, { value: 'ab' } )).eql(true);
+    });
 
-		it('#different values', function() {
-			expect(mm._match( { value: 'ab' }, { value: 'cd' } )).eql(false);
-		});
+    it('#different values', function() {
+      expect(mm._match( { value: 'ab' }, { value: 'cd' } )).eql(false);
+    });
 
-	});
-
-
-	describe('#primitive $eq', function() {
-
-		it('#same values', function() {
-			expect(mm._match( { value: 'ab' }, { value: { $eq: 'ab' } } )).eql(true);
-		});
-
-		it('#different values', function() {
-			expect(mm._match( { value: 'ab' }, { value: { $eq: 'cd' } } )).eql(false);
-		});
-
-	});
+  });
 
 
-	describe('#primitive $ne', function() {
+  describe('#primitive $eq', function() {
 
-		it('#same values', function() {
-			expect(mm._match( { value: 'ab' }, { value: { $ne: 'ab' } } )).eql(false);
-		});
+    it('#same values', function() {
+      expect(mm._match( { value: 'ab' }, { value: { $eq: 'ab' } } )).eql(true);
+    });
 
-		it('#different values', function() {
-			expect(mm._match( { value: 'ab' }, { value: { $ne: 'cd' } } )).eql(true);
-		});
+    it('#different values', function() {
+      expect(mm._match( { value: 'ab' }, { value: { $eq: 'cd' } } )).eql(false);
+    });
 
-	});
-
-
-	describe('#primitive $gt', function() {
-
-		it('#greater', function() {
-			expect(mm._match( { value: 1 }, { value: { $gt: 0 } } )).eql(true);
-		});
-
-		it('#equal', function() {
-			expect(mm._match( { value: 1 }, { value: { $gt: 1 } } )).eql(false);
-		});
-
-		it('#less', function() {
-			expect(mm._match( { value: 1 }, { value: { $gt: 2 } } )).eql(false);
-		});
-
-	});
+  });
 
 
-	describe('#primitive $gte', function() {
+  describe('#primitive $ne', function() {
 
-		it('#greater', function() {
-			expect(mm._match( { value: 1 }, { value: { $gte: 0 } } )).eql(true);
-		});
+    it('#same values', function() {
+      expect(mm._match( { value: 'ab' }, { value: { $ne: 'ab' } } )).eql(false);
+    });
 
-		it('#equal', function() {
-			expect(mm._match( { value: 1 }, { value: { $gte: 1 } } )).eql(true);
-		});
+    it('#different values', function() {
+      expect(mm._match( { value: 'ab' }, { value: { $ne: 'cd' } } )).eql(true);
+    });
 
-		it('#less', function() {
-			expect(mm._match( { value: 1 }, { value: { $gte: 2 } } )).eql(false);
-		});
-
-	});
+  });
 
 
-	describe('#primitive $lt', function() {
+  describe('#primitive $gt', function() {
 
-		it('#greater', function() {
-			expect(mm._match( { value: 1 }, { value: { $lt: 0 } } )).eql(false);
-		});
+    it('#greater', function() {
+      expect(mm._match( { value: 1 }, { value: { $gt: 0 } } )).eql(true);
+    });
 
-		it('#equal', function() {
-			expect(mm._match( { value: 1 }, { value: { $lt: 1 } } )).eql(false);
-		});
+    it('#equal', function() {
+      expect(mm._match( { value: 1 }, { value: { $gt: 1 } } )).eql(false);
+    });
 
-		it('#less', function() {
-			expect(mm._match( { value: 1 }, { value: { $lt: 2 } } )).eql(true);
-		});
+    it('#less', function() {
+      expect(mm._match( { value: 1 }, { value: { $gt: 2 } } )).eql(false);
+    });
 
-	});
-
-
-	describe('#primitive $lte', function() {
-
-		it('#greater', function() {
-			expect(mm._match( { value: 1 }, { value: { $lte: 0 } } )).eql(false);
-		});
-
-		it('#equal', function() {
-			expect(mm._match( { value: 1 }, { value: { $lte: 1 } } )).eql(true);
-		});
-
-		it('#less', function() {
-			expect(mm._match( { value: 1 }, { value: { $lte: 2 } } )).eql(true);
-		});
-
-	});
+  });
 
 
-	describe('#primitive $in', function() {
+  describe('#primitive $gte', function() {
 
-		it('#first', function() {
-			expect(mm._match( { value: 'ab' }, { value: { $in: [ 'ab', 'cd' ] } } )).eql(true);
-		});
+    it('#greater', function() {
+      expect(mm._match( { value: 1 }, { value: { $gte: 0 } } )).eql(true);
+    });
 
-		it('#second', function() {
-			expect(mm._match( { value: 'cd' }, { value: { $in: [ 'ab', 'cd' ] } } )).eql(true);
-		});
+    it('#equal', function() {
+      expect(mm._match( { value: 1 }, { value: { $gte: 1 } } )).eql(true);
+    });
 
-		it('#none', function() {
-			expect(mm._match( { value: 'de' }, { value: { $in: [ 'ab', 'cd' ] } } )).eql(false);
-		});
+    it('#less', function() {
+      expect(mm._match( { value: 1 }, { value: { $gte: 2 } } )).eql(false);
+    });
 
-	});
+  });
 
 
-	describe('#primitive $in', function() {
+  describe('# primitive $lt', function() {
 
-		it('#first', function() {
-			expect(mm._match( { value: 'ab' }, { value: { $nin: [ 'ab', 'cd' ] } } )).eql(false);
-		});
+    describe('# greater', function() {
+      it('# numbers', function() {
+        expect(mm._match( { value: 1 }, { value: { $lt: 0 } } )).eql(false);
+      });
+      it('# strings', function() {
+        expect(mm._match( { value: 'ab' }, { value: { $lt: 'aa' } } )).eql(false);
+      });
+    });
 
-		it('#second', function() {
-			expect(mm._match( { value: 'cd' }, { value: { $nin: [ 'ab', 'cd' ] } } )).eql(false);
-		});
+    describe('# equal', function() {
+      it('# numbers', function() {
+        expect(mm._match( { value: 1 }, { value: { $lt: 1 } } )).eql(false);
+      });
+      it('# strings', function() {
+        expect(mm._match( { value: 'ab' }, { value: { $lt: 'ab' } } )).eql(false);
+      });
+    });
 
-		it('#none', function() {
-			expect(mm._match( { value: 'de' }, { value: { $nin: [ 'ab', 'cd' ] } } )).eql(true);
-		});
+    describe('# less', function() {
+      it('# numbers', function() {
+        expect(mm._match( { value: 1 }, { value: { $lt: 2 } } )).eql(true);
+      });
+      it('# strings', function() {
+        expect(mm._match( { value: 'ab' }, { value: { $lt: 'ac' } } )).eql(true);
+      });
+    });
 
-	});
+  });
+
+
+  describe('# $lte', function() {
+    describe('# primitive', function() {
+
+      describe('# greater', function() {
+        it('# numbers', function() {
+          expect(mm._match( { value: 1 }, { value: { $lte: 0 } } )).eql(false);
+        });
+        it('# strings', function() {
+          expect(mm._match( { value: 'ab' }, { value: { $lte: 'aa' } } )).eql(false);
+        });
+      });
+
+      describe('# equal', function() {
+        it('# numbers', function() {
+          expect(mm._match( { value: 1 }, { value: { $lte: 1 } } )).eql(true);
+        });
+        it('# strings', function() {
+          expect(mm._match( { value: 'ab' }, { value: { $lte: 'ab' } } )).eql(true);
+        });
+      });
+
+      describe('# less', function() {
+        it('# numbers', function() {
+          expect(mm._match( { value: 1 }, { value: { $lte: 2 } } )).eql(true);
+        });
+        it('# strings', function() {
+          expect(mm._match( { value: 'ab' }, { value: { $lte: 'ac' } } )).eql(true);
+        });
+      });
+
+    });
+
+    describe('# compound field', function() {
+
+      describe('# greater', function() {
+        it('# numbers', function() {
+          expect(mm._match( { a: { b: 1 } }, { 'a.b': { $lte: 0 } } )).eql(false);
+        });
+      });
+
+      describe('# equal', function() {
+        it('# numbers', function() {
+          expect(mm._match( { a: { b: 1 } }, { 'a.b': { $lte: 1 } } )).eql(true);
+        });
+      });
+
+      describe('# less', function() {
+        it('# numbers', function() {
+          expect(mm._match( { a: { b: 1 } }, { 'a.b': { $lte: 2 } } )).eql(true);
+        });
+      });
+
+    });
+  });
+
+
+  describe('#primitive $in', function() {
+
+    it('#first', function() {
+      expect(mm._match( { value: 'ab' }, { value: { $in: [ 'ab', 'cd' ] } } )).eql(true);
+    });
+
+    it('#second', function() {
+      expect(mm._match( { value: 'cd' }, { value: { $in: [ 'ab', 'cd' ] } } )).eql(true);
+    });
+
+    it('#none', function() {
+      expect(mm._match( { value: 'de' }, { value: { $in: [ 'ab', 'cd' ] } } )).eql(false);
+    });
+
+  });
+
+
+  describe('#primitive $in', function() {
+
+    it('#first', function() {
+      expect(mm._match( { value: 'ab' }, { value: { $nin: [ 'ab', 'cd' ] } } )).eql(false);
+    });
+
+    it('#second', function() {
+      expect(mm._match( { value: 'cd' }, { value: { $nin: [ 'ab', 'cd' ] } } )).eql(false);
+    });
+
+    it('#none', function() {
+      expect(mm._match( { value: 'de' }, { value: { $nin: [ 'ab', 'cd' ] } } )).eql(true);
+    });
+
+  });
 
 
 });
