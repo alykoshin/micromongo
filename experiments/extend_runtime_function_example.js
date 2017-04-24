90ef19314766bffe0b7646b8c9bcce3880b68d29
Alexander <alykoshin@gmail.com>
2017-04-25 01:24:33 +0300

    minor fix

--- experiments/extend_runtime_function_example.js
+++ experiments/extend_runtime_function_example.js
@@ -1,18 +1,17 @@
 /**
  *
- * This file is an example how to extend the functionality of micromongo in runtime
- * It shows how to add new function $sortAlphaNumeric to aggregation framework
- * or replace existing $sort function.
+ * This file is an example how to extend the functionality of `micromongo` in runtime
+ *
+ * It shows how to add new function `$userFn` to CRUD `match` functions
+ * and use it in `find` function
  *
  */
 
 "use strict";
 
 
-//const _ = require('lodash');
-const mm = require('../');
-const match = require('../lib/crud/match');
 //const mm = require('micromongo');
+const mm = require('../');
 
 const data = [
   { user: { Name: 'User1234',  UserId: '1234' } },
@@ -22,7 +21,6 @@ const data = [
 console.log('Extending postOperators with new $userFn function...');
 
 mm._crud._match.postOperators.$userFn = function(doc, query) {
-//match.postOperators.$userFn = function(doc, query) {
   console.log('Inside $userFn(): doc', doc, ' query:', query);
   return doc.Name !== 'User'+doc.UserId;
 };
